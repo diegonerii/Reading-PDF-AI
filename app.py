3fda5d649adcce9a3303a3db8301e1550af23b8b
Diego Neri <diego.holanda.neri@gmail.com>
2024-06-08 13:10:27 -0300

    Versão Simplificada

--- app.py
+++ app.py
@@ -1,33 +1,29 @@
-import pandas as pd
+import fitz
 import streamlit as st
 
 from dotenv import find_dotenv, load_dotenv
-from utils.summarize_text import *
+from utils.summarize_text import summaryText
 
 load_dotenv(find_dotenv())
 
-st.title('Seja Bem-Vindo!')
-
-# Options list
-df_format_file = pd.DataFrame({
-    'Input': ['.pdf']
-    })
-
-option = st.selectbox(
-    'Qual o Formato dos Dados de Entrada?',
-     df_format_file['Input'])
+st.title('Seja bem-vindo ao Leitor de PDF por AI!')
 
 file = st.file_uploader("Por favor, Selecione o Arquivo", type=['pdf'])
+if file:
+    binary_data = file.getvalue()
+    doc = fitz.open(stream=binary_data, filetype="pdf")
+    text = ""
+    for page in doc:
+        text += page.get_text()
 
-text_input = st.text_input(
-        "Faça uma Pergunta Sobre o Texto"
-    )
+    text_input = st.text_input(
+            "Faça uma Pergunta Sobre o Texto"
+        )
 
-if text_input and file:
-    summary = summaryText(media=file, question=text_input)
-    summarized_text = summary()
-    # print(summarized_text)
-
-    if st.button('Gerar Reposta'):
+    if text_input and file:
+        summary = summaryText(text=text, question=text_input)
+        summarized_text = summary()
         st.write(summarized_text)
+
+
 
